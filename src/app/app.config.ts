e6f0a4e1df86816829b2e267df1c4e034e70f7ba
Marcelo Iglesias - Linux A515 <marceloagustiniglesias@gmail.com>
2024-08-16 11:11:59 +0100

    Added hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop'), provideHttpClient()]
+  providers: [provideRouter(routes, withHashLocation()), provideAnimationsAsync('noop'), provideHttpClient()]
 };
